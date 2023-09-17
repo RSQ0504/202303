118d4e8a688f074e479d2860d5a4a43fc0ee08a4
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-09-17 16:24:32 -0700

    update

--- project1/python/relu.py
+++ project1/python/relu.py
@@ -10,8 +10,9 @@ def relu_forward(input_data):
 
     ###### Fill in the code here ######
     # Replace the following line with your implementation.
-    output['data'] = np.zeros_like(input_data['data'])
-    
+    output['data'] = input_data['data']
+    output["data"][output["data"]<0]=0
+
     return output
 
 def relu_backward(output, input_data, layer):
@@ -20,3 +21,14 @@ def relu_backward(output, input_data, layer):
     input_od = np.zeros_like(input_data['data'])
 
     return input_od
+
+'''
+if __name__=="__main__":
+    input_data = {}
+    input_data['data'] = np.array([-1, -4, 0, 2, 3, 4, 5, -6])
+    input_data['width'] = 5
+    input_data['height'] = 5
+    input_data['channel'] = 3
+    input_data['batch_size'] = 4
+    relu_forward(input_data)
+'''
