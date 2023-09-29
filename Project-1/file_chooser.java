d0bc3bfd88618db63a948007bfa71cf8eda8cc68
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-09-29 09:33:22 -0700

    backup

--- Project-1/file_chooser.java
+++ Project-1/file_chooser.java
@@ -11,26 +11,36 @@ public class file_chooser {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 400);
 
-        JButton button = new JButton("Open .wav File");
-        button.setBounds(150, 70, 150, 30);
+        JButton button_wav = new JButton("Open .wav File");
+        button_wav.setBounds(150, 70, 150, 30);
 
-        JFileChooser file_window = new JFileChooser();
+        JButton button_tif = new JButton("Open .tif File");
+        button_tif.setBounds(150, 120, 150, 30);
 
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("WAV", "wav");
-        file_window.setFileFilter(filter);
+        JFileChooser file_window_wav = new JFileChooser();
+
+        FileNameExtensionFilter filter_wav = new FileNameExtensionFilter("WAV", "wav");
+        file_window_wav.setFileFilter(filter_wav);
+
+
+        JFileChooser file_window_tif = new JFileChooser();
+
+        FileNameExtensionFilter filter_tif = new FileNameExtensionFilter("TIF", "tif");
+        file_window_tif.setFileFilter(filter_tif);
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); 
-        panel.add(button);
+        panel.add(button_wav);
+        panel.add(button_tif);
         frame.add(panel);
         frame.setVisible(true);
 
-        button.addActionListener(new ActionListener() {
+        button_wav.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int check = file_window.showOpenDialog(null);
+                int check = file_window_wav.showOpenDialog(null);
                 if (check == JFileChooser.APPROVE_OPTION) {
-                    File file = file_window.getSelectedFile();
+                    File file = file_window_wav.getSelectedFile();
                     System.out.println("Selected .wav File: " + file.getPath());
                     frame.getContentPane().removeAll();
                     read_wave reader = new read_wave(file.getPath());
@@ -39,7 +49,20 @@ public class file_chooser {
                     frame.revalidate();
                     frame.repaint();
                 } else {
-                    System.out.println("Wrong format");
+                    System.out.println("Cancel");
+                }
+            }
+        });
+
+        button_tif.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int check = file_window_tif.showOpenDialog(null);
+                if (check == JFileChooser.APPROVE_OPTION) {
+                    File file = file_window_tif.getSelectedFile();
+                    System.out.println("Selected .tif File: " + file.getPath());
+                } else {
+                    System.out.println("cancel");
                 }
             }
         });
