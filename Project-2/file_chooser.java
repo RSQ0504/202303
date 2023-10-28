17af7e4a50c87f2b28fd4636ff54c7a0d8ea9bfa
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-10-28 10:57:49 -0700

    backup

---
+++ Project-2/file_chooser.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class file_chooser {
+    private static read_wave wav_reader;
+    private static read_tif tif_reader;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Project 1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JButton button_wav = new JButton("Open .wav File");
+        button_wav.setBounds(150, 70, 200, 100);
+
+        JButton button_tif = new JButton("Open .tif File");
+        button_tif.setBounds(150, 120, 200, 100);
+
+        JButton button_back1 = new JButton("back");
+        button_tif.setBounds(150, 500, 200, 100);
+
+        JButton button_back2 = new JButton("back");
+        button_tif.setBounds(150, 500, 200, 100);
+
+        JButton button_exit = new JButton("exit");
+        button_tif.setBounds(150, 120, 200, 100);
+
+        JFileChooser file_window_wav = new JFileChooser();
+
+        FileNameExtensionFilter filter_wav = new FileNameExtensionFilter("WAV", "wav");
+        file_window_wav.setFileFilter(filter_wav);
+
+
+        JFileChooser file_window_tif = new JFileChooser();
+
+        FileNameExtensionFilter filter_tif = new FileNameExtensionFilter("TIF", "tif");
+        file_window_tif.setFileFilter(filter_tif);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); 
+        panel.add(button_wav);
+        panel.add(button_tif);
+        panel.add(button_exit);
+        frame.add(panel);
+        frame.setVisible(true);
+
+        button_wav.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int check = file_window_wav.showOpenDialog(null);
+                if (check == JFileChooser.APPROVE_OPTION) {
+                    File file = file_window_wav.getSelectedFile();
+                    System.out.println("Selected .wav File: " + file.getPath());
+                    //frame.getContentPane().removeAll();
+                    wav_reader = new read_wave(file.getPath());
+                    wav_reader.draw();
+                    wav_reader.add(button_back1);
+                    frame.add(wav_reader);
+                    panel.setVisible(false);
+                } else {
+                    System.out.println("Cancel");
+                }
+            }
+        });
+
+        button_tif.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int check = file_window_tif.showOpenDialog(null);
+                if (check == JFileChooser.APPROVE_OPTION) {
+                    File file = file_window_tif.getSelectedFile();
+                    System.out.println("Selected .tif File: " + file.getPath());
+                    tif_reader = new read_tif(file.getPath());
+                    tif_reader.draw();
+                    tif_reader.add(button_back2);
+                    frame.add(tif_reader);
+                    panel.setVisible(false);
+                } else {
+                    System.out.println("cancel");
+                }
+            }
+        });
+        button_back1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                wav_reader.setVisible(false);
+                panel.setVisible(true);
+                
+            }
+        });
+
+        button_back2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tif_reader.setVisible(false);
+                panel.setVisible(true);
+                
+            }
+        });
+
+        button_exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ Project-2/read_wave.java
@@ -0,0 +1,101 @@
+import java.awt.*;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+public class read_wave  extends JPanel {
+
+    private static int bytes_per_sample;
+    private static int sample_rate;
+    private static byte[] data_c1;
+    private static byte[] data_c2;
+    private static byte[] total_data;
+    private static AudioFormat audioFormat;
+
+
+    public read_wave(String filePath) {
+        try {
+            // Open the audio file
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
+            audioFormat = audioInputStream.getFormat();
+            
+            bytes_per_sample = audioFormat.getSampleSizeInBits() / 8;
+            sample_rate = (int) audioFormat.getSampleRate();
+            int frame_num = (int) audioInputStream.getFrameLength();
+            int byte_num_per_channel = frame_num * bytes_per_sample;
+
+            data_c1 = new byte[byte_num_per_channel];
+            data_c2 = new byte[byte_num_per_channel];
+            total_data = new byte[byte_num_per_channel*2];
+            audioInputStream.read(total_data);
+
+            for (int i = 0; i < byte_num_per_channel; i+=bytes_per_sample) {
+                for(int j = 0; j<bytes_per_sample;j++){
+                    data_c1[i+j] = total_data[i * 2 + j];
+                    data_c2[i+j] = total_data[i * 2 + bytes_per_sample + j];
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void draw(){
+        repaint(); 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int w = getWidth();
+        int h = getHeight();
+        int w_drawing = getWidth();
+        int h_drawing = getHeight() - 35;
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, w, h);
+
+        int sample_num = data_c1.length / bytes_per_sample;
+        int sample_num_total = total_data.length / bytes_per_sample;
+        double unit_x = (double) w_drawing / sample_num;
+        double unit_y = (double) (h_drawing/2) / (1 << (bytes_per_sample * 8 - 1));
+
+        for (int i = 0; i < sample_num - 1; i++) {
+            int x1 = (int) (i * unit_x);
+            int x2 = (int) ((i + 1) * unit_x);
+            int y1;
+            int y2;
+            if(audioFormat.isBigEndian()){
+                y1 = (int) (ByteBuffer.wrap(data_c1, i * bytes_per_sample, bytes_per_sample).order(ByteOrder.BIG_ENDIAN).getShort() * unit_y);
+                y2 = (int) (ByteBuffer.wrap(data_c1, (i + 1) * bytes_per_sample, bytes_per_sample).order(ByteOrder.BIG_ENDIAN).getShort() * unit_y);
+            }else{
+                y1 = (int) (ByteBuffer.wrap(data_c1, i * bytes_per_sample, bytes_per_sample).order(ByteOrder.LITTLE_ENDIAN).getShort() * unit_y);
+                y2 = (int) (ByteBuffer.wrap(data_c1, (i + 1) * bytes_per_sample, bytes_per_sample).order(ByteOrder.LITTLE_ENDIAN).getShort() * unit_y);
+            }
+
+            g.setColor(Color.GREEN);
+            g.drawLine(x1, h_drawing / 4 + y1 + 35, x2, h_drawing / 4 + y2 + 35);
+
+            if(audioFormat.isBigEndian()){
+                y1 = (int) (ByteBuffer.wrap(data_c2, i * bytes_per_sample, bytes_per_sample).order(ByteOrder.BIG_ENDIAN).getShort() * unit_y);
+                y2 = (int) (ByteBuffer.wrap(data_c2, (i + 1) * bytes_per_sample, bytes_per_sample).order(ByteOrder.BIG_ENDIAN).getShort() * unit_y);
+            }else{
+                y1 = (int) (ByteBuffer.wrap(data_c2, i * bytes_per_sample, bytes_per_sample).order(ByteOrder.LITTLE_ENDIAN).getShort() * unit_y);
+                y2 = (int) (ByteBuffer.wrap(data_c2, (i + 1) * bytes_per_sample, bytes_per_sample).order(ByteOrder.LITTLE_ENDIAN).getShort() * unit_y);
+            }
+
+            g.setColor(Color.GREEN);
+            g.drawLine(x1, 3 * h_drawing / 4 + y1 + 35, x2, 3 * h_drawing / 4 + y2 + 35);
+        }
+
+        g.setColor(Color.white);
+        g.drawString("number of the samples (per channel): " + sample_num, 30, 20);
+        g.drawString("total number of the samples: " + sample_num_total, 30, 40);
+        g.drawString("sampling frequency: " + sample_rate + " Hz", 30, 60);
+    }
+}
