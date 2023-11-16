1a0a7958e40557c05cfc7d2cfba02d8190b6f500
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-11-15 18:09:58 -0800

    backup

---
+++ Assignment3/my_malloc.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <math.h>
+#include "list.h"
+#include "my_malloc.h"
+
+int height(Block* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return node->height;
+}
+
+int getBalance(Block* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return height(node->left) - height(node->right);
+}
+
+Block* rightRotate(Block* y) {
+    Block* x = y->left;
+    Block* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + fmax(height(y->left), height(y->right));
+    x->height = 1 + fmax(height(x->left), height(x->right));
+
+    return x;
+}
+
+Block* leftRotate(Block* x) {
+    Block* y = x->right;
+    Block* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + fmax(height(x->left), height(x->right));
+    y->height = 1 + fmax(height(y->left), height(y->right));
+
+    return y;
+}
+
+Block* free_tree_insert(Block* root, Block* insert_block) {
+    if (root == NULL) {
+        return insert_block;
+    }
+    if (insert_block->size < root->size) {
+        root->left = insert(root->left, insert_block);
+    } else if (insert_block->size > root->size) {
+        root->right = insert(root->right, insert_block);
+    } else {
+        root->depth = insert_block;
+        return root;
+    }
+
+    root->height = 1 + fmax(get_height(root->left), get_height(root->right));
+
+    int balance = get_balance(root);
+    if (balance > 1 && insert_block->size < root->left->size) {
+        return right_rotation(root);
+    }
+    if (balance < -1 && insert_block->size > root->right->size) {
+        return left_rotation(root);
+    }
+    if (balance > 1 && insert_block->size > root->left->size) {
+        root->left = left_rotation(root->left);
+        return right_rotation(root);
+    }
+    if (balance < -1 && insert_block->size < root->right->size) {
+        root->right = right_rotation(root->right);
+        return left_rotation(root);
+    }
+    return root;
+}
+
+Block* free_tree_delete(Block* root, Block* del) {
+    if (root == NULL) {
+        return NULL;
+    }
+    if (del->size < root->size) {
+        root->left = free_tree_delete(root->left, del);
+    } else if (del->size > root->size) {
+        root->right = free_tree_delete(root->right, del);
+    } else {
+        // Case 1: Node with one child or no child
+        if (root->left == NULL || root->right == NULL) {
+            Block* temp = (root->left != NULL) ? root->left : root->right;
+            // No child case
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            } else {
+                // One child case
+                *root = *temp; // Copy the contents of the non-empty child
+            }
+            free(temp);
+        } else {
+            // Case 2: Node with two children
+            Block* temp = findMin(root->right);
+            root->size = temp->size;
+            root->depth = temp->depth;
+            root->right = free_tree_delete(root->right, temp);
+        }
+    }
+
+    if (root == NULL) {
+        return NULL;
+    }
+
+    root->height = 1 + fmax(get_height(root->left), get_height(root->right));
+
+    int balance = get_balance(root);
+    if (balance > 1 && get_balance(root->left) >= 0) {
+        return right_rotation(root);
+    }
+    if (balance > 1 && get_balance(root->left) < 0) {
+        root->left = left_rotation(root->left);
+        return right_rotation(root);
+    }
+    if (balance < -1 && get_balance(root->right) <= 0) {
+        return left_rotation(root);
+    }
+    if (balance < -1 && get_balance(root->right) > 0) {
+        root->right = right_rotation(root->right);
+        return left_rotation(root);
+    }
+    return root;
+}
+
+Block* findMin(Block* node) {
+    while (node->left != NULL) {
+        node = node->left;
+    }
+    return node;
+}
---
+++ Assignment3/my_malloc.h
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "list.h"
+
+
+#define INITIAL_BLOCK_SIZE 1024
+
+
+typedef struct Block_mem Block;
+
+struct Block_mem
+{
+    void* start;
+    bool free;
+    size_t size;
+
+    // for tree
+    Block* right;
+    Block* left;
+    int height;
+
+    // store the same size block in the same position in tree
+    // by creating a 3D depth
+    Block* depth;
+};
+
+
+int get_balance(Block * block);
+
+int get_height(Block * block);
+
+Block* right_rotation(Block* y);
+
+Block* left_rotation(Block* x);
+
+Block* free_tree_insert(Block* root, Block* insert);
+
+
+
+void* mem_init();
+
+void* my_malloc(size_t size);
+
+void my_free(void *ptr);
