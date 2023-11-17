246d2e12c5a528a5044514c0c63d97ac42b49b96
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-11-17 11:33:57 -0800

    backup

--- Assignment3/mem_test.c
+++ Assignment3/mem_test.c
@@ -13,7 +13,13 @@ int main(){
     Block* block4 = (Block*)malloc(sizeof(Block));
     block4->size =20;
     Block* block5 = (Block*)malloc(sizeof(Block));
-    block5->size =70;
+    block5->size =15;
+    Block* block6 = (Block*)malloc(sizeof(Block));
+    block6->size =12;
+    Block* block7 = (Block*)malloc(sizeof(Block));
+    block7->size =16;
+    Block* block8 = (Block*)malloc(sizeof(Block));
+    block8->size =8;
     
     Block* root = NULL;
     root = free_tree_insert(root,block1);
@@ -21,15 +27,30 @@ int main(){
     root = free_tree_insert(root,block3);
     root = free_tree_insert(root,block4);
     root = free_tree_insert(root,block5);
+    root = free_tree_insert(root,block6);
+    root = free_tree_insert(root,block7);
+    root = free_tree_insert(root,block8);
+    
 
     printDotFormat(root);
     printf("\n\n\n");
     
     root = free_tree_delete(root, block4);
+    printDotFormat(root);
+    printf("\n\n\n");
     root = free_tree_delete(root, block5);
+    printDotFormat(root);
+
+
+    free(block1);
+    free(block2);
+    free(block3);
     free(block4);
     free(block5);
-    printDotFormat(root);
+    free(block6);
+    free(block7);
+    free(block8);
+
 
     return 0;
 }
