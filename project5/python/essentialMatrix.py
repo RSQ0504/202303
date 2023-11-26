d16c67e160e9720784f13db6e50d6febc9d6b42a
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-11-26 11:22:59 -0800

    backup

--- project5/python/essentialMatrix.py
+++ project5/python/essentialMatrix.py
@@ -9,5 +9,11 @@ def essentialMatrix(F, K1, K2):
     Returns:
         E:  Essential Matrix  
     """
-    E = None
+    if F.shape != (3, 3):
+        raise ValueError("Invalid shape for the fundamental matrix F")
+    if K1.shape != (3, 3) or K2.shape != (3, 3):
+        raise ValueError("Invalid shape for camera matrices K1 or K2")
+
+    E = np.dot(K2.T, np.dot(F, K1))
+
     return E
