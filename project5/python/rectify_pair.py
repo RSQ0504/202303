8db368b31006b25e2d41e685d9a70629e76a40b2
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-11-30 10:12:11 -0800

    backuop

--- project5/python/rectify_pair.py
+++ project5/python/rectify_pair.py
@@ -10,5 +10,29 @@ def rectify_pair(K1, K2, R1, R2, t1, t2):
 
     M1, M2, K1n, K2n, R1n, R2n, t1n, t2n = None, None, None, None, None, None, None, None
 
+    c1 = np.dot(-np.linalg.inv(np.dot(K1, R1)), (np.dot(K1, t1)))
+    c2 = np.dot(-np.linalg.inv(np.dot(K2, R2)), (np.dot(K2, t2)))
+
+    r1 = np.abs(c1 - c2) / np.linalg.norm(c1 - c2)
+
+    r21 = np.cross(R1[2, :], r1)
+    r21 = r21 / np.linalg.norm(r21)
+    r31 = np.cross(r1, r21)
+    r31 = r31 / np.linalg.norm(r31)
+    R1n = np.vstack((r1, r21, r31))
+
+    r22 = np.cross(R2[2, :], r1)
+    r32 = np.cross(r1, r22)
+    R2n = np.vstack((r1, r22, r32))
+
+    K1n = K2
+    K2n = K2
+
+    t1n = -np.dot(R1n, c1)
+    t2n = -np.dot(R2n, c2)
+
+    M1 = np.dot(np.dot(K1n, R1n), np.linalg.inv(np.dot(K1, R1)))
+    M2 = np.dot(np.dot(K2n, R2n), np.linalg.inv(np.dot(K2, R2)))
+
     return M1, M2, K1n, K2n, R1n, R2n, t1n, t2n
 
