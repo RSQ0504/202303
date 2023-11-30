f9387266a0fc9587c07a6a07a8ab07cb74e36baa
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-11-30 12:59:02 -0800

    backup

--- project5/python/estimate_pose.py
+++ project5/python/estimate_pose.py
@@ -17,12 +17,6 @@ def estimate_pose(x, X):
     x_new = np.vstack((x, np.ones((1, N))))
     X_new = np.vstack((X, np.ones((1, N))))
 
-    x_mean = np.mean(x_new[:2], axis=1)
-    X_mean = np.mean(X_new[:3], axis=1)
-
-    x_new[:2] -= x_mean.reshape(-1, 1)
-    X_new[:3] -= X_mean.reshape(-1, 1)
-
     A = np.zeros((2 * N, 12))
     for i in range(N):
         A[2 * i, :4] = X_new[:, i]
@@ -33,12 +27,5 @@ def estimate_pose(x, X):
     _, _, V = np.linalg.svd(A)
 
     P = V[-1, :].reshape(3, 4)
-
-    T_x = np.eye(3)
-    T_x[:2, 2] = -x_mean
-    T_X = np.eye(4)
-    T_X[:3, 3] = X_mean
-
-    P = np.linalg.inv(T_x) @ P @ T_X
 
     return P
